0815ced6069d39c1cdfe25a32e1d149cb920be97
guilherme <gsfl3101@gmail.com>
2022-09-10 20:40:17 +0200

    Added oauth2 using jwt token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
-from app import database, schemas, models, utils
+from app import database, schemas, models, utils, oauth2
 
 
 router = APIRouter(tags=["Authentication"])
@@ -22,4 +22,5 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.ge
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials"
         )
     # create a JWT token
-    return {"token": "example token"}
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
