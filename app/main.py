720e14c18b80352f51b981fe6fab277c1bd54e9a
guilherme <gsfl3101@gmail.com>
2022-09-12 22:22:04 +0200

    Removed default postres drive connect

--- app/main.py
+++ app/main.py
@@ -1,29 +1,9 @@
 from fastapi import FastAPI
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 from app import models
 from app.database import engine
 from .routers import user, auth, post
 
 models.Base.metadata.create_all(bind=engine)
-
-while True:
-    try:
-        conn = psycopg2.connect(
-            host="localhost",
-            database="fastapi",
-            user="postgres",
-            password="example",
-            cursor_factory=RealDictCursor,
-        )
-        cursor = conn.cursor()
-        print("Database connection was successfull!")
-        break
-    except Exception as error:
-        print("Connecting to databse failed")
-        print("Error: ", error)
-        time.sleep(2)
 
 app = FastAPI()
 app.include_router(auth.router)
