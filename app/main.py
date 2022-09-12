1d82e35669a0e3a8eeb384afa7ab9a26caf7e65f
guilherme <gsfl3101@gmail.com>
2022-09-12 22:48:34 +0200

    Added configuration to use env vars

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from app import models
+from app import models, config
 from app.database import engine
 from .routers import user, auth, post
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 app.include_router(auth.router)
 app.include_router(post.router)
 app.include_router(user.router)
